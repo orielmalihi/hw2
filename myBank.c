9f1d4f7fc61f3e0be9258e4e39cffe8aa5a9bd26
oriel <orielmalihi1@gmail.com>
2019-11-26 11:21:24 +0200

    Define upgrades

--- myBank.c
+++ myBank.c
@@ -4,13 +4,17 @@
  *  Created on: 19 Nov 2019
  *      Author: oriel
  */
-#define NumOfAccounts 50
-#define AccountStatus 2
+#define Num_Of_Accounts 50
+#define Account_Info 2
+#define Account_status 0
+#define Account_balance 1
+#define CLOSE 0
+#define OPEN 1
 
 #include "stdio.h"
 
 
-double bank[AccountStatus][NumOfAccounts] = {{0},{0}};
+double bank[Account_Info][Num_Of_Accounts] = {{0},{0}};
 
 void clearInputBuffer(){
 	char c = 'a';
@@ -21,16 +25,16 @@ void clearInputBuffer(){
 
 void open(double amount){
 	int accountNumber;
-	for(int i = 0; i<NumOfAccounts; i++){
-		if(bank[0][i]==0.0){
-			bank[0][i]=1.0;
+	for(int i = 0; i<Num_Of_Accounts; i++){
+		if(bank[Account_status][i]==CLOSE){
+			bank[Account_status][i]=OPEN;
 			accountNumber = i + 901;
 			if(amount<0){
 				printf("Depoisit was unsuccessful.\nCan not deposit a negative amount.\n");
 				return;
 			}
-			bank[1][i] += amount;
-			printf("your account number is %d , and your current balance is %0.2lf\n", accountNumber, bank[1][i]);
+			bank[Account_balance][i] += amount;
+			printf("your account number is %d , and your current balance is %0.2lf\n", accountNumber, bank[Account_balance][i]);
 			return;
 		}
 
@@ -44,11 +48,11 @@ void balance(int accountNumber){
 		printf("This account is not in our bank\n");
 		return;
 	}
-	if(bank[0][accountNumber]==0.0){
+	if(bank[Account_status][accountNumber]==CLOSE){
 		printf("This account is closed\n");
 	}
 	else{
-		printf("your current balance is %0.2lf\n", bank[1][accountNumber]);
+		printf("your current balance is %0.2lf\n", bank[Account_balance][accountNumber]);
 	}
 }
 
@@ -58,16 +62,16 @@ void deposit(int accountNumber, double amount){
 		printf("This account is not in our bank\n");
 		return;
 	}
-	if(bank[0][accountNumber]==0){
+	if(bank[Account_status][accountNumber]==CLOSE){
 		printf("This account is closed\n");
 		return;
 	}else if(amount<0){
-			printf("Depoisit was unsuccessful.\nCan not deposit a negative amount\n");
-			return;
-		}
-		bank[1][accountNumber] += amount;
-		printf("your current balance is %0.2lf\n", bank[1][accountNumber]);
+		printf("Depoisit was unsuccessful.\nCan not deposit a negative amount\n");
+		return;
 	}
+	bank[Account_balance][accountNumber] += amount;
+	printf("your current balance is %0.2lf\n", bank[Account_balance][accountNumber]);
+}
 
 
 void withrow(int accountNumber, double amount){
@@ -76,21 +80,21 @@ void withrow(int accountNumber, double amount){
 		printf("This account is not in our bank\n");
 		return;
 	}
-	if(bank[0][accountNumber]==0){
+	if(bank[Account_status][accountNumber]==CLOSE){
 		printf("This account is closed\n");
 		return;
 	}else if(amount<0){
-			printf("Withrow was unsuccessful.\nCan not withrow a negative amount\n");
-			return;
-		}
-		bank[1][accountNumber] -= amount;
-		if(bank[1][accountNumber]<0.0){
-			bank[1][accountNumber] += amount;
-			printf("Withrow was unsuccessful.\nYou do not have enugh money in this account.\n");
-			return;
-		}
-		printf("your current balance is %0.2lf\n", bank[1][accountNumber]);
+		printf("Withrow was unsuccessful.\nCan not withrow a negative amount\n");
+		return;
 	}
+	bank[Account_balance][accountNumber] -= amount;
+	if(bank[Account_balance][accountNumber]<0.0){
+		bank[Account_balance][accountNumber] += amount;
+		printf("Withrow was unsuccessful.\nYou do not have enugh money in this account.\n");
+		return;
+	}
+	printf("your current balance is %0.2lf\n", bank[Account_balance][accountNumber]);
+}
 
 
 void close(int accountNumber){
@@ -99,9 +103,9 @@ void close(int accountNumber){
 		printf("This account is not in our bank\n");
 		return;
 	}
-	if(bank[0][accountNumber]==1.0){
-		bank[0][accountNumber]=0.0;
-		bank[1][accountNumber]=0.0;
+	if(bank[Account_status][accountNumber]==OPEN){
+		bank[Account_status][accountNumber]=CLOSE;
+		bank[Account_balance][accountNumber]=0;
 		printf("Closing the account was successful\n");
 	}
 	else{
@@ -112,26 +116,26 @@ void close(int accountNumber){
 void interestRate(double interestRate){
 	interestRate /= 100;
 	interestRate += 1;
-	for(int i = 0; i<NumOfAccounts; i++){
-		if(bank[0][i]==1.0)
-			bank[1][i] *= interestRate;
+	for(int i = 0; i<Num_Of_Accounts; i++){
+		if(bank[Account_status][i]==OPEN)
+			bank[Account_balance][i] *= interestRate;
 	}
 }
 
 void printAccounts(){
-	for(int i = 0; i<NumOfAccounts; i++){
-		if(bank[0][i]==1.0){
+	for(int i = 0; i<Num_Of_Accounts; i++){
+		if(bank[Account_status][i]==OPEN){
 			int accountNumber = i+901;
-			printf("Account number %d has balance of %0.2lf $\n", accountNumber, bank[1][i]);
+			printf("Account number %d has balance of %0.2lf $\n", accountNumber, bank[Account_balance][i]);
 		}
 	}
 }
 
 void end(){
-	for(int i = 0; i<NumOfAccounts; i++){
-		if(bank[0][i]==1.0){
-			bank[0][i]=0.0;
-			bank[1][i]=0.0;
+	for(int i = 0; i<Num_Of_Accounts; i++){
+		if(bank[Account_status][i]==OPEN){
+			bank[Account_status][i]=CLOSE;
+			bank[Account_balance][i]=0;
 		}
 	}
 	printf("All accounts have been closed successfully.\nGood bye!\n");
